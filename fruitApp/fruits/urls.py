0643accc8367cd268902e600f4070c8a82e23b76
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-24 17:24:24 +0200

    adding paths and functions

--- fruitApp/fruits/urls.py
+++ fruitApp/fruits/urls.py
@@ -4,6 +4,14 @@ from django.urls import path, include
 from fruitApp.fruits import views
 
 urlpatterns = [
-    path('', views.index, name='')
+    path('', views.index, name=''),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('create/fruit/', views.create_fruit, name='dashboard'),
+    path('<int:fruit_pk>/', include([
+        path('details/', views.detail_fruit, name='detail-fruit'),
+        path('edit/', views.edit_fruit, name='edit-fruit'),
+        path('delete/', views.delete_fruit, name='delete-fruit'),
+    ])),
+    path('category/create', views.create_category, name='create-category'),
 
 ]
