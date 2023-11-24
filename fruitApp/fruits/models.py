bf55c764f2ddc9340f8c411c448a470a5b9b87ce
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-24 16:02:33 +0200

    added category and fruit models

--- fruitApp/fruits/models.py
+++ fruitApp/fruits/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Fruit(models.Model):
+    name = models.CharField(
+        max_length=30,
+        validators=[
+            MinLengthValidator(2),
+        ]
+    )
+    image_url = models.URLField()
+    description = models.TextField()
+    nutrition = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
